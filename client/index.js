9b26c26e516bbb2883e8f61e9f4bed8c03f1a3b4
Julian Ćwirko <julian.cwirko@gmail.com>
2015-12-07 20:37:31 +0100

    added full text logo

--- client/index.js
+++ client/index.js
@@ -8,11 +8,23 @@ const dataURItoBlobData = function (dataURI) {
     return new Uint8Array(array);
 };
 
+
+const getTemplateInstanceByDOM = function (domElemQuetySelector) {
+    const blazeView = Blaze.getView(document.querySelector(domElemQuetySelector));
+    return blazeView && blazeView.templateInstance();
+};
+
 const openModal = function (e, tmpl, modalTemplate) {
     e.preventDefault();
-    Session.set('modalTemplate', modalTemplate);
+    tmpl.modalTemplate.set(modalTemplate);
     $('html').css('overflow', 'hidden');
     tmpl.modalActive.set(true);
+};
+
+const closeModal = function () {
+    const tmpl = getTemplateInstanceByDOM('.jumbo');
+    $('html').css('overflow', 'auto');
+    tmpl.modalActive.set(false);
 };
 
 const blockLetters = function (e) {
@@ -20,6 +32,13 @@ const blockLetters = function (e) {
     if (!checkKey) {
         $(e.currentTarget).val('');
     };
+};
+
+const changeLogoColor = function (e, tmpl, query) {
+    const value = $(e.currentTarget).val();
+    if (value && /^#(?:[0-9a-f]{3}){1,2}$/i.test(value)) {
+        tmpl.$('.logo-custom svg').find(query).css('fill', value);
+    }
 }
 
 
@@ -30,6 +49,7 @@ const blockLetters = function (e) {
 
 Template.main.onCreated(function () {
     this.modalActive = new ReactiveVar(false);
+    this.modalTemplate = new ReactiveVar('');
 });
 Template.main.events({
     'click .js-choose-color'(e, tmpl) {
@@ -50,12 +70,25 @@ Template.main.events({
         const svgElem = document.querySelector('#meteor-logo-svg svg');
         const doc = new XMLSerializer().serializeToString(svgElem);
         saveAs(new Blob([doc], {type: 'image/svg+xml;charset=utf-8'}), 'meteor-logo.svg');
+    },
+    'click .js-download-switch-logo'(e) {
+        e.preventDefault();
+        const logoTmpl = Session.get('activeSVGTemplate');
+        if (typeof logoTmpl === 'undefined' ||logoTmpl === 'logoSVG') {
+            Session.set('activeSVGTemplate', 'logoTextSVG');
+        }
+        if (logoTmpl === 'logoTextSVG') {
+            Session.set('activeSVGTemplate', 'logoSVG');
+        }
     }
 });
 Template.main.helpers({
     isModalActive() {
         const tmpl = Template.instance();
         return tmpl.modalActive.get();
+    },
+    logoTypeClass() {
+        return Session.get('activeSVGTemplate') || 'logoSVG';
     }
 });
 
@@ -67,7 +100,8 @@ Template.main.helpers({
 
 Template.modal.helpers({
     modalTemplate() {
-        return Session.get('modalTemplate') || '';
+        const mainTmpl = getTemplateInstanceByDOM('.jumbo');
+        return mainTmpl.modalTemplate.get() || '';
     }
 });
 
@@ -82,24 +116,43 @@ Template.colorTmpl.helpers({
         let inp = document.createElement('input');
         inp.setAttribute('type', 'color');
         return inp.type !== 'text';
+    },
+    isOnlyLogo() {
+        const typeOfLogo = Session.get('activeSVGTemplate');
+        if (typeof typeOfLogo === 'undefined' || typeOfLogo === 'logoSVG') {
+            return true;
+        }
     }
 });
 Template.colorTmpl.events({
-    'change .js-choose-color-input'(e, tmpl) {
-        const value = $(e.currentTarget).val();
-        if (value && /^#(?:[0-9a-f]{3}){1,2}$/i.test(value)) {
-            tmpl.$('.logo-custom svg').find('path').css('fill', value);
-        }
+    'change .js-choose-all-color'(e, tmpl) {
+        changeLogoColor(e, tmpl, 'path');
     },
-    'click .js-logo-choose'(e) {
+    'change .js-choose-text-color'(e, tmpl) {
+        changeLogoColor(e, tmpl, 'g:eq(1) path');
+    },
+    'change .js-choose-logo-color'(e, tmpl) {
+        changeLogoColor(e, tmpl, 'g:eq(0) path');
+    },
+    'click .js-logo-choose-all'(e) {
         const svgPath = $(e.currentTarget).find('svg path');
         const fill = svgPath && svgPath.css('fill');
-        let blazeView, tmpl;
+        let tmpl;
         if (fill) {
             $('#meteor-logo-svg svg').find('path').css('fill', fill);
-            blazeView = Blaze.getView(document.querySelector('.jumbo'));
-            tmpl = blazeView && blazeView.templateInstance();
-            tmpl.modalActive.set(false);
+            closeModal();
+        }
+    },
+    'click .js-logo-text-choose'(e) {
+        const svgPathText = $(e.currentTarget).find('svg g:eq(1) path');
+        const svgPathLogo = $(e.currentTarget).find('svg g:eq(0) path');
+        const fillText = svgPathText && svgPathText.css('fill');
+        const fillLogo = svgPathLogo && svgPathLogo.css('fill');
+        let tmpl;
+        if (fillText && fillLogo) {
+            $('#meteor-logo-svg svg').find('g:eq(0) path').css('fill', fillLogo);
+            $('#meteor-logo-svg svg').find('g:eq(1) path').css('fill', fillText);
+            closeModal();
         }
     }
 });
@@ -113,7 +166,7 @@ Template.colorTmpl.events({
 Template.pngTmpl.events({
     'click .js-download-png-logo'(e, tmpl) {
         e.preventDefault();
-        let domElem, clonedElem, width, height, pngHeightInput, pngWidthInput, blazeView, mainTmpl;
+        let domElem, clonedElem, width, height, pngHeightInput, pngWidthInput, mainTmpl;
         pngWidthInput = $('[name=png-file-width]').val();
         pngHeightInput = $('[name=png-file-height]').val();
         domElem = document.querySelector('#meteor-logo-svg svg');
@@ -133,9 +186,7 @@ Template.pngTmpl.events({
         clonedElem.toDataURL('image/png', {
             callback: function (data) {
                 saveAs(new Blob([dataURItoBlobData(data)], {type: 'image/png;charset=utf-8'}), 'meteor-logo.png');
-                blazeView = Blaze.getView(document.querySelector('.jumbo'));
-                mainTmpl = blazeView && blazeView.templateInstance();
-                mainTmpl.modalActive.set(false);
+                closeModal();
             }
         });
     },
@@ -152,5 +203,17 @@ Template.pngTmpl.events({
         if ($(e.currentTarget).val() === '') {
             $('[name=png-file-width]').attr('placeholder', 'Width');
         }
+    }
+});
+
+
+// =========================
+// Template: svgLogo
+// =========================
+
+
+Template.svgLogo.helpers({
+    svgTmpl() {
+        return Session.get('activeSVGTemplate') || 'logoSVG';
     }
 });
